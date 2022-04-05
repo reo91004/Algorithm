636a784e19bcca1e205ce93d23fc663a53446157
reo91004 <reo91004@gmail.com>
2022-04-05 17:12:05 +0900

    22.04.05 업데이트

---
+++ [BOJ] 백준 풀이/단계별로 풀어보기 (step by step)/#8 기본 수학 1/10757_큰 수 A+B (22.04.05 성공).cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string A, B;
+    cin >> A >> B;
+
+    int len = max(A.size(), B.size());
+    vector<int> v;
+    bool carry = false;
+    int i, tmp;
+
+    for (i = 0; i < len; i++) {
+        tmp = carry;
+        carry = false;
+
+        if (i < A.size()) tmp += A[A.size() - i - 1] - '0';
+        if (i < B.size()) tmp += B[B.size() - i - 1] - '0';
+
+        if (tmp >= 10) {
+            carry = true;
+            tmp -= 10;
+        }
+
+        v.insert(v.begin(), tmp);
+    }
+
+    if (carry) v.insert(v.begin(), 1);
+
+    for (int i = 0; i < v.size(); ++i) cout << v[i];
+}
