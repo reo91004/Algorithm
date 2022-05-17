b05aa1ee278232d50ae8d5258e33a9b6fcc6e651
reo91004 <reo91004@gmail.com>
2022-05-17 23:50:28 +0900

    22.05.17 업데이트

---
+++ [BOJ] 백준 풀이/단계별로 풀어보기 (step by step)/#12 집합과 맵/1269_대칭 차집합 (22.05.17 성공).py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+setA = set(map(int, input().split()))
+setB = set(map(int, input().split()))
+
+print(len(setA - setB) + len(setB - setA))
