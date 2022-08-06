2fd29ccf131938b538fca959fa20b0c1eb48d2f9
reo91004 <reo91004@gmail.com>
2022-08-06 15:34:22 +0900

    22.08.06 업데이트

---
+++ [BOJ] 백준 풀이/단계별로 풀어보기 (step by step)/#15 백트래킹/14888_연산자 끼워넣기 (22.08.06 성공).cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int N;
+int arr[11];
+int operators[4];  // 덧셈, 뺄셈, 곱셈, 나눗셈
+int res_min = INT_MAX;
+int res_max = INT_MIN;
+
+void dfs(int cur, int res) {
+    if (cur == N) {
+        res_min = std::min(res_min, res);
+        res_max = std::max(res_max, res);
+        return;
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        if (operators[i] == 0)
+            continue;
+
+        operators[i]--;
+
+        if (i == 0)
+            dfs(cur + 1, res + arr[cur]);
+        else if (i == 1)
+            dfs(cur + 1, res - arr[cur]);
+        else if (i == 2)
+            dfs(cur + 1, res * arr[cur]);
+        else if (i == 3)
+            dfs(cur + 1, res / arr[cur]);
+        
+        operators[i]++;
+    }
+}
+
+void init() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+int main() {
+    init();
+
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i) 
+        std::cin >> arr[i];
+    for (int i = 0; i < 4; ++i) 
+        std::cin >> operators[i];
+
+    dfs(1, arr[0]);
+
+    std::cout << res_max << "\n" << res_min;
+
+    return 0;
+}
