d7fade6f2c2e36e54fcadee2925f2ab71d888c76
reo91004 <reo91004@gmail.com>
2022-09-04 18:45:31 +0900

    22.09.04 업데이트

---
+++ [BOJ] 백준 풀이/단계별로 풀어보기 (step by step)/#01 입출력과 사칙연산/10171_고양이 (22.09.04 성공).py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    print("\\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
---
+++ [BOJ] 백준 풀이/단계별로 풀어보기 (step by step)/#01 입출력과 사칙연산/25083_새싹 (22.09.04 성공).py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
