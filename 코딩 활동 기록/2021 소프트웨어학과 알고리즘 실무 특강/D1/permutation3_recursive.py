08a856984154694ac767b30a5c7c5e28c808fe3e
reo91004 <reo91004@gmail.com>
2024-03-17 16:36:27 +0900

    fix: 파일 정리

--- 코딩 활동 기록/2021 소프트웨어학과 알고리즘 실무 특강/D1/permutation3_recursive.py
+++
@@ -1,9 +0,0 @@
-def permute(s):
-  out = []
-  if len(s) == 1:
-    return s
-  else:
-    for i,let in enumerate(s):
-      for perm in permute(s[:i] + s[i+1:]):
-        out += [let + perm]
-  return out
