5bc38d74e72d97bd60f5851afdfc3540eea9f7be
reo91004 <reo91004@gmail.com>
2024-04-03 04:25:33 +0900

    폴더 구조 변경

--- C/main.cpp
+++ C/main.cpp
@@ -1,30 +1,7 @@
-#include <avr/io.h>
+#include <iostream>
 
-void main() {
-    PORTB |= 0b11110000;
-    DDRB |= 0b11110000;
-    DDRD &= 0b11000011;
+int main() {
+    printf("hello\n");
 
-    while (1) {
-        if (PIND & 0x04) {
-            PORTB |= 0x10;
-        } else {
-            PORTB &= ~0x10;
-        }
-        if (PIND & 0x08) {
-            PORTB |= 0x20;
-        } else {
-            PORTB &= ~0x20;
-        }
-        if (PIND & 0x10) {
-            PORTB |= 0x40;
-        } else {
-            PORTB &= ~0x40;
-        }
-        if (PIND & 0x20) {
-            PORTB |= 0x80;
-        } else {
-            PORTB &= ~0x80;
-        }
-    }
+    return 0;
 }
